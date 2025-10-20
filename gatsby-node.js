d16186204d4833fc5af83dd94042fa9dd0178a73
reisjoaov <joaoreisvictor@hotmail.com>
2025-10-19 22:17:35 -0300

    .

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,9 +2,9 @@ import fetch from "node-fetch";
 
 export const sourceNodes = async ({actions, createNodeId, createContentDigest}) => {
     const {createNode} = actions;
+
     const usersResponse = await fetch("https://jsonplaceholder.typicode.com/users");
     const users = await usersResponse.json();
-
     users.forEach(user => {
         createNode({
             ...user, //notação Spread
@@ -17,4 +17,22 @@ export const sourceNodes = async ({actions, createNodeId, createContentDigest}) 
             }
         });
     });
+
+    const postResponse = await fetch("https://jsonplaceholder.typicode.com/posts")
+    const posts = await postResponse.json();
+
+    posts.forEach(post => {
+        createNode({
+            ...post, //notação Spread
+            id: createNodeId(`Post-${post.id}`),
+            parent: null,
+            children: [],
+            internal: {
+                type: "Post",
+                contentDigest: createContentDigest(post),
+            }
+        });
+    });
+
+
 }
