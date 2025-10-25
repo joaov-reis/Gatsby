a46bbded86521373f0b4de6044addcfdcab2e342
reisjoaov <joaoreisvictor@hotmail.com>
2025-10-24 22:39:12 -0300

    -

--- src/pages/{mdx.frontmatter__slug}.js
+++ src/pages/{mdx.frontmatter__slug}.js
@@ -1,24 +1,32 @@
 import React from 'react';
 import { graphql } from "gatsby"
 import Layout from '../components/layout';
+import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 
 export default function BlogPost ({ data, children }) {
+
+  const image = getImage(data.mdx.frontmatter.hero_image);
   
   return (
     <Layout>
       <h2>{data.mdx.frontmatter.title}</h2>
       <p>Data: {data.mdx.frontmatter.date}</p>
+      <GatsbyImage image={getImage(data.mdx.frontmatter.hero_image)} alt={data.mdx.frontmatter.hero_image_alt}/>
       {children}
     </Layout>
   )
 }
 
 export const query = graphql`
-  query($id: String) {
-    mdx(id: {eq: $id}) {
+  query ($id: String) {
+    mdx(id: { eq: $id }) {
       frontmatter {
         title
         date(formatString: "DD/MM/YYYY")
+        hero_image_alt
+        hero_image_credit_link
+        hero_image_credit_text
+        hero_image
       }
     }
   }
